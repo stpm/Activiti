1243f0b1f489aa747a3b64ad5d9e7c3aceda02dc
Sean Patterson-McGuire <stm@google.com>
2015-10-28 15:27:16 -0700

    previous commit just added ExpandedBpmnParse.Builder-equivalent functionality to ExpandedDeployment.Builder; this removes it from ExpandedBpmnParse.

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/deployer/ExpandedBpmnParse.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/deployer/ExpandedBpmnParse.java
@@ -1,16 +1,11 @@
 package org.activiti.engine.impl.bpmn.deployer;
 
 import org.activiti.engine.impl.bpmn.parser.BpmnParse;
-import org.activiti.engine.impl.bpmn.parser.BpmnParser;
-import org.activiti.engine.impl.cmd.DeploymentSettings;
-import org.activiti.engine.impl.persistence.entity.DeploymentEntity;
 import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.activiti.engine.impl.persistence.entity.ResourceEntity;
 
-import java.io.ByteArrayInputStream;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Map;
 
 public class ExpandedBpmnParse {
   /**
@@ -45,53 +40,5 @@ public class ExpandedBpmnParse {
   
   public ResourceEntity getResource() {
     return resource;
-  }
-  
-  public static class Builder {
-    private final DeploymentEntity deployment;
-    private final BpmnParser bpmnParser;
-    private final Map<String, Object> deploymentSettings;
-    
-    public Builder(DeploymentEntity deployment, BpmnParser bpmnParser, Map<String, Object> deploymentSettings) {
-      this.deployment = deployment;
-      this.bpmnParser = bpmnParser;
-      this.deploymentSettings = deploymentSettings;
-    }
-    
-    public ExpandedBpmnParse buildParseForResource(ResourceEntity resource) {
-      ByteArrayInputStream inputStream = new ByteArrayInputStream(resource.getBytes());
-      BpmnParse executedParse = createBpmnParse(resource.getName(), inputStream);
-      
-      return new ExpandedBpmnParse(executedParse, resource);
-    }
-    
-    protected BpmnParse createBpmnParse(String resourceName, ByteArrayInputStream inputStream) {
-      BpmnParse bpmnParse = bpmnParser.createParse()
-          .sourceInputStream(inputStream)
-          .setSourceSystemId(resourceName)
-          .deployment(deployment)
-          .name(resourceName);
-
-      if (deploymentSettings != null) {
-
-        // Schema validation if needed
-        if (deploymentSettings.containsKey(DeploymentSettings.IS_BPMN20_XSD_VALIDATION_ENABLED)) {
-          bpmnParse.setValidateSchema((Boolean) deploymentSettings.get(DeploymentSettings.IS_BPMN20_XSD_VALIDATION_ENABLED));
-        }
-
-        // Process validation if needed
-        if (deploymentSettings.containsKey(DeploymentSettings.IS_PROCESS_VALIDATION_ENABLED)) {
-          bpmnParse.setValidateProcess((Boolean) deploymentSettings.get(DeploymentSettings.IS_PROCESS_VALIDATION_ENABLED));
-        }
-
-      } else {
-        // On redeploy, we assume it is validated at the first
-        // deploy
-        bpmnParse.setValidateSchema(false);
-        bpmnParse.setValidateProcess(false);
-      }
-      bpmnParse.execute();
-      return bpmnParse;
-    }
   }
 }
